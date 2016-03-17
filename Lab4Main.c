595276fc4611080173406e934d33c82908665b11
shojimonk <shojimonk@gmail.com>
2016-03-17 19:22:41 -0400

    attempt to implement struct sending through zmq
    attempted to implement struct sending through zmq, but is all commented
    out. reverted to previous string sending method.

--- Lab4Main.c
+++ Lab4Main.c
@@ -1,10 +1,34 @@
 
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <zmq.h>
+
+/*  unimplemented struct for eventual use in zmq send/recv
+struct workerDataSend {
+	int numThreads;
+	int wordLength;
+	char startWord[20];
+	char endWord[20];
+	char hash[32];
+};
+
+struct workerDataSend* newDataSender() {
+	struct workerDataSend *newSender = malloc(sizeof(struct workerDataSend));
+	if (newSender == NULL) {
+		return NULL;
+	}
+
+	return newSender;
+}
+
+void delWorkerData(struct workerDataSend *wDS) {
+	if (wDS != NULL) {
+		free(wDS);
+	}
+}
+*/
 
 /* If the last character of the passed string is a newline, removes the newline. */
 void chomp(char *toChomp) {
@@ -22,8 +46,8 @@ void wordBuilder(int increment, int numSlaves, int wordLength, int divided[3], c
 	uint16_t tempChar;
 	wordResult[0] = (increment * divided[0]) + 32;
 	tempChar = increment * divided[1];
-	if (tempChar >= 95) {
-		wordResult[0] += tempChar / 95;
+	if (tempChar >= 94) {
+		wordResult[0] += tempChar / 94;
 		wordResult[1] = (tempChar % 95) + 32;
 	}
 	else {
@@ -39,7 +63,6 @@ void wordBuilder(int increment, int numSlaves, int wordLength, int divided[3], c
 		wordResult[j] = 32;
 	}
 	wordResult[wordLength] = '\0';
-	//printf("result = %s\t is: '%d ' %d'\n", wordResult, wordResult[0], wordResult[1]);
 	return;
 }
 
@@ -141,14 +164,45 @@ int main(void)
 	printf("Press Enter when the workers are ready: ");
 	getchar();
 	printf("preparing to send to here: %s\n", slaveIPs[0]);
+	/*
+	struct workerDataSend *toSend = newDataSender();
 
+	//toSend = (struct WorkerDataSend*)malloc(sizeof(struct WorkerDataSend));
+	
+	toSend->numThreads = slaveThreads;
+	toSend->wordLength = goalLength;
+	strcpy(toSend->hash, theHash);
+	*/
 	for (int i = 0; i < numSlaves; i++) {
 		char workerConnect[255] = { '\0' };
 		strcat(workerConnect, "tcp://");
 		strncat(workerConnect, slaveIPs[i], strlen(slaveIPs[i])-1);
 		strcat(workerConnect, ":6534");
 		printf("\nconnecting to: %s\n", workerConnect);
+		
 		zmq_connect(sender, workerConnect);
+		/*
+		char startWord[20];
+		wordBuilder(i*slaveThreads, numSlaves, goalLength, divided, startWord);
+
+		strcpy(toSend->startWord, startWord);
+
+		if (i != numSlaves - 1)
+			wordBuilder((i + 1) * slaveThreads, numSlaves, goalLength, divided, startWord);
+		else
+			for (int j = 0; j < goalLength; j++)
+				startWord[j] = 126;
+
+		strcpy(toSend->endWord, startWord);
+
+		printf("sizeof toSend: %d\n", sizeof(toSend));
+		printf("sending end Start numT wordL : ..%s..%s..%d..%d..\n", toSend->endWord, toSend->startWord, toSend->numThreads, toSend->wordLength);
+
+		printf("main connected...\n");
+		zmq_send(sender, &toSend, sizeof(&toSend), 0);
+		printf("main sent...\n");
+		*/
+		
 		char info[255];
 		
 		sprintf(info, "%d", slaveThreads);
@@ -161,14 +215,19 @@ int main(void)
 
 		char startWord[50];
 		wordBuilder(i*slaveThreads, numSlaves, goalLength, divided, startWord);
+		printf("startWord = ..%s..\n", startWord);
 		zmq_send(sender, startWord, goalLength+1, 0);	// send starting word
 
-		wordBuilder((i + 1) * slaveThreads, numSlaves, goalLength, divided, startWord);
+		if (i != numSlaves - 1)
+			wordBuilder((i + 1) * slaveThreads, numSlaves, goalLength, divided, startWord);
+		else
+			for (int j = 0; j < goalLength; j++) 
+				startWord[j] = 126;
+		//printf("endWord = ..%s..\n", startWord);
 		zmq_send(sender, startWord, goalLength + 1, 0);	// send ending word
 
 		zmq_send(sender, theHash, 33, 0);		// send the hash to be matched
-
-		// workerCall ( numThreads, passLength, startWord, count, targetHash );
+		
 	}
 	
 	printf("waiting for return...\n");
