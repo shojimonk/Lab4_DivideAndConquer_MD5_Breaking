a00538e7d44ab04f47f981ed8eddfaabf62d627d
shojimonk <shojimonk@gmail.com>
2016-03-17 19:46:32 -0400

    implemented mutex and conditions. fixed bugs in MD5 implementation and wordIncrement.
    Fixed bugs in MD5 implementation and wordIncrement, so threads now
    properly work through words, and successfully detect matches.
    
    Implemented mutex and conditions. condition triggers properly, but
    currently mutex is locked, then released, but nothing that was waiting
    on the mutex unblocks once it is available.

--- Lab4Worker.c
+++ Lab4Worker.c
@@ -14,6 +14,11 @@
 
 typedef enum { false, true } bool;
 
+pthread_cond_t checkCondition;
+pthread_mutex_t matchedMutex;
+int8_t finished = -1;
+char *match;
+
 struct workerThreadData {
 	int increment;
 	int wordLength;
@@ -24,69 +29,107 @@ struct workerThreadData {
 };
 
 void wordIncrement(char* theWord, int increment, int index) {
-	if (theWord[index] < 126 + increment) {
+	if ((int)theWord[index] <= (126 - increment)) {
 		theWord[index] += increment;
 		return;
 	}
 	else {
+		wordIncrement(theWord, 1, index - 1);
 		theWord[index] = (theWord[index] + increment) % 126;
-		wordIncrement(theWord, 1, index + 1);
+		theWord[index] += 32;
 	}
 }
 
 // The method run in each thread to loop through the words and check hash results.
 void* workerThread(void* data) {
-					// initialize data
+	// initialize data
+	//pthread_mutex_lock(&matchedMutex);
 	struct workerThreadData *inputs;
 	inputs = (struct workerThreadData*) data;
+
+	printf("in%d\n", inputs->threadID);
+	fflush(stdout);
+
 	char* currentWord = (char*)malloc(sizeof(char) * strlen(inputs->startWord + 1));
 	strcpy(currentWord, inputs->startWord);
 	char* currentHash;
-	MD5_CTX context;
-	unsigned char digest[16];
+	
+	int counter = 0;
 
-	wordIncrement(currentWord, inputs->threadID, strlen(currentWord));	// pre-increment by thread # to prevent all threads running same words.
-	// printf(".. %d\n", inputs->threadID);
+	wordIncrement(currentWord, inputs->threadID, strlen(currentWord) - 1);	// pre-increment by thread # to prevent all threads running same words.
 	while (true) {
-		currentHash = (char*)malloc(sizeof(char) * strlen(inputs->match + 1));
-		unsigned int len = strlen(currentWord);
-		MDInit(&context);
-		MDUpdate(&context, currentWord, len);
-		MDFinal(digest, &context);
-		for (int i = 0; i < 16; i++) {
-			char twoHex[2];
-			sprintf(twoHex, "%02x", digest[i]);
-			strcat(currentHash, twoHex);
+		if (finished < 0) {
+			//printf("..%d..\n", inputs->threadID);
+			//pthread_mutex_unlock(&matchedMutex);
+			MD5_CTX context;
+			unsigned char digest[16];
+			currentHash = (char*)malloc(sizeof(char) * strlen(inputs->match + 1));
+			unsigned int len = strlen(currentWord);
+			MDInit(&context);
+			MDUpdate(&context, currentWord, len);
+			MDFinal(digest, &context);
+			for (int i = 0; i < 16; i++) {
+				char twoHex[2];
+				sprintf(twoHex, "%02x", digest[i]);
+				strcat(currentHash, twoHex);
+			}
+			if (strcmp(currentHash, inputs->match) == 0) {					// if match has been found.
+				printf("worker '%d' found match.\tword is: \"%s\"\n", inputs->threadID, currentWord);
+				printf("wait lock %d\n", inputs->threadID);
+				pthread_mutex_lock(&matchedMutex);
+				printf("got lock %d\n", inputs->threadID);
+				finished = inputs->threadID;
+				pthread_cond_signal(&checkCondition);
+				strcpy(match, currentWord);
+				free(currentWord);
+				free(currentHash);
+				pthread_mutex_unlock(&matchedMutex);
+				printf("release lock %d\n", inputs->threadID);
+				pthread_exit(NULL);
+				// return match somehow
+			}
+			int comp = strcmp(currentWord, inputs->endWord);
+			if (comp >= 0) {		// return failure to find match, and done.
+				printf("failed to match? .%d.\n", inputs->threadID);
+				printf("current word: %s\tend Word: %s\n", currentWord, inputs->endWord);
+				fflush(stdout);
+				printf("wait lock %d\n", inputs->threadID);
+				pthread_mutex_lock(&matchedMutex);
+				printf("got lock %d\n", inputs->threadID);
+				free(currentWord);
+				free(currentHash);
+				finished = inputs->threadID;
+				pthread_cond_signal(&checkCondition);
+				pthread_mutex_unlock(&matchedMutex);
+				printf("release lock %d\n", inputs->threadID);
+				pthread_exit(NULL);
+			}
+			else {					// increment to next word.
+				comp = 0;
+				wordIncrement(currentWord, inputs->increment, strlen(currentWord) - 1);
+				counter++;
+			}
+			//pthread_mutex_lock(&matchedMutex);
+			currentHash[0] = '\0';
+			continue;
 		}
-		if (strcmp(currentHash, inputs->match) == 0) {					// if match has been found.
-			//printf("worker found match.\tword is: %s\n", currentWord);
-			//printf("orig hash = %s\t new hash = %s\n", inputs->match, currentHash);
-			char* result = (char*)malloc(sizeof(char) * (strlen(currentWord) + 1));
-			strcpy(result, currentWord);
-			//strcat(result, '/0');
-			free(currentWord);
-			free(currentHash);
-			pthread_exit(result);
-			// return match somehow
-		}
-		int comp = strcmp(currentWord, inputs->endWord);
-		if (comp >= 0) {		// return failure to find match, and done.
-			free(currentWord);
-			free(currentHash);
-			pthread_exit(NULL);
-		}
-		else {					// increment to next word.
-			wordIncrement(currentWord, inputs->increment, 0);
-		}
-		currentHash[0] = '\0';
-		free(currentHash);
+		else printf("..%d.fini is..%d.\n", inputs->threadID, finished);
+		break;
 	}
+	printf("wait lock %d\n", inputs->threadID);
+	pthread_mutex_lock(&matchedMutex);
+	printf("got lock %d\n", inputs->threadID);
+	printf("exit%d\t%d\n", inputs->threadID, finished);
+	finished = inputs->threadID;
+	pthread_cond_signal(&checkCondition);
+	pthread_mutex_unlock(&matchedMutex);
+	printf("release lock %d\n", inputs->threadID);
 	pthread_exit(NULL);
 }
 
 // 
 int errCheck(int result) {
-	if (result != 0) {
+	if (result == -1) {
 		fprintf(stderr, "error reading from file.\n");
 		return EXIT_FAILURE;
 	}
@@ -104,6 +147,7 @@ int main(void)
 	zmq_connect(sender, "tcp://localhost:6535");
 
 	printf("worker waiting...\n");
+
 	int result = zmq_recv(receiver, buff, 255, 0);		// receive # of threads to use
 	errCheck(result);
 	int numThreads = atoi(buff);
@@ -127,11 +171,15 @@ int main(void)
 	errCheck(result);
 	strcpy(endWord, buff);
 	printf("Recieved end: \n%s\n", endWord);
-	
+
 	result = zmq_recv(receiver, buff, 255, 0);			// receive hash to match
 	errCheck(result);
 	strcpy(theHash, buff);
 	printf("Recieved hash: \n%s\n", theHash);
+	
+
+	pthread_cond_init(&checkCondition, NULL);
+	pthread_mutex_init(&matchedMutex, NULL);
 
 	struct workerThreadData threadData[numThreads];
 	pthread_t workers[numThreads];
@@ -142,13 +190,29 @@ int main(void)
 		threadData[i].endWord = endWord;
 		threadData[i].match = theHash;
 		threadData[i].threadID = i;
-		printf("pr%d\n", i);
+		printf("pr%d\n", threadData[i].threadID);
 		pthread_create(&workers[i], NULL, workerThread, (void *)&threadData[i]);
 	}
-	
+
+	match = (char*)malloc(sizeof(char) * wordLength);
 	// returning results from threads...
-
-
+	for (int threads = numThreads-1; threads > 0; threads--) {
+		pthread_cond_wait(&checkCondition, &matchedMutex);
+		printf("wait lock main\n");
+		pthread_mutex_lock(&matchedMutex);
+		pthread_join(workers[finished], NULL);
+		if (match != NULL) {
+			finished = 10;
+		}
+		else {
+			printf("match!=null\n");
+			finished = -1;
+		}
+		printf("waiting on: %d\n", threads);
+		pthread_mutex_unlock(&matchedMutex);
+	}
+	
+	
 	// sending results to master...
 	zmq_send(sender, "test", 5, 0);
 
