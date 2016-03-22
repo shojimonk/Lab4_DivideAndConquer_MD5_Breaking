313542e717df28b7be355b410a299ba30228364f
shojimonk <shojimonk@gmail.com>
2016-03-22 11:01:52 -0400

    Cleaned up code
    improved documentation of methods. added print for first workers
    returned result.

--- Lab4Main.c
+++ Lab4Main.c
@@ -40,7 +40,15 @@ void chomp(char *toChomp) {
 	return;
 }
 
-
+/********************************************************************************	
+*	Builds word to be sent to workers based on inputs							*
+**	Inputs **																	*
+*	increment: which increment of the total is currently being assigned			*
+*	numSlaves: total number of increments to be divided amongst					*
+*	wordLength: number of characters in the word								*
+*	divided[3]: 3 ints that specify how much to increment the 3 most sig chars	*
+*	wordResult:	char array used to store the built word							*
+*********************************************************************************/
 void wordBuilder(int increment, int numSlaves, int wordLength, int divided[3], char *wordResult) {
 
 	uint16_t tempChar;
@@ -66,11 +74,12 @@ void wordBuilder(int increment, int numSlaves, int wordLength, int divided[3], c
 	return;
 }
 
-/*
-	divides the workload up based on the number of workers and threads per worker.
-	If not an even division, the last worker is also given the remainder, which 
-	is never over 1% more than the rest of the workers.
-*/
+
+/****************************************************************************************
+*	Divides the workload up based on the number of workers and threads per worker.		*
+*	If not an even division, the last worker is also given the remainder, which			*
+*	is never over 1% more than the rest of the workers.									*
+*****************************************************************************************/
 int* divisionOfLabour(uint8_t numSlaves, uint8_t numThreads, uint8_t passLength) {
 
 	int slaveThreads = numSlaves * numThreads;
@@ -92,8 +101,6 @@ int main(void)
 	FILE *fp;
 	char buff[255];
 	char *verify;
-
-	printf("Starting Main.\n");
 
 	// open config file
 	fp = fopen("./config.txt", "r");
@@ -120,7 +127,7 @@ int main(void)
 		slaveIPs[i] = (char*)malloc(sizeof(char) * strlen(buff));
 		strcpy(slaveIPs[i], buff);
 		chomp(slaveIPs[i]);
-		printf("initial slave IP %d = %s\n", i, slaveIPs[i]);
+//		printf("initial slave IP %d = %s\n", i, slaveIPs[i]);
 	}
 
 	verify = fgets(buff, 255, fp);		// read in number of threads per worker
@@ -150,8 +157,7 @@ int main(void)
 	chomp(theHash);
 	printf("the hash = %s\n", theHash);
 
-									// calculate contents of work packets
-	int *divided = divisionOfLabour(numSlaves, slaveThreads, goalLength);
+	int *divided = divisionOfLabour(numSlaves, slaveThreads, goalLength);	// calculate contents of work packets
 	for (int loop = 0; loop < 3; loop++) {	
 		printf("\tdivided %d = %d\n", loop, divided[loop]);
 	}
@@ -163,16 +169,7 @@ int main(void)
 
 	printf("Press Enter when the workers are ready: ");
 	getchar();
-	printf("preparing to send to here: %s\n", slaveIPs[0]);
-	/*
-	struct workerDataSend *toSend = newDataSender();
-
-	//toSend = (struct WorkerDataSend*)malloc(sizeof(struct WorkerDataSend));
 	
-	toSend->numThreads = slaveThreads;
-	toSend->wordLength = goalLength;
-	strcpy(toSend->hash, theHash);
-	*/
 	for (int i = 0; i < numSlaves; i++) {
 		char workerConnect[255] = { '\0' };
 		strcat(workerConnect, "tcp://");
@@ -181,10 +178,10 @@ int main(void)
 		printf("\nconnecting to: %s\n", workerConnect);
 		
 		zmq_connect(sender, workerConnect);
-		/*
+		
+		/*	// non-working code attempting to send data as a struct through zmq. needs data-serialization, outside of scope.
 		char startWord[20];
 		wordBuilder(i*slaveThreads, numSlaves, goalLength, divided, startWord);
-
 		strcpy(toSend->startWord, startWord);
 
 		if (i != numSlaves - 1)
@@ -194,17 +191,14 @@ int main(void)
 				startWord[j] = 126;
 
 		strcpy(toSend->endWord, startWord);
-
 		printf("sizeof toSend: %d\n", sizeof(toSend));
 		printf("sending end Start numT wordL : ..%s..%s..%d..%d..\n", toSend->endWord, toSend->startWord, toSend->numThreads, toSend->wordLength);
-
 		printf("main connected...\n");
 		zmq_send(sender, &toSend, sizeof(&toSend), 0);
 		printf("main sent...\n");
 		*/
 		
 		char info[255];
-		
 		sprintf(info, "%d", slaveThreads);
 		printf("threads = %s\t", info);
 		zmq_send(sender, info, strlen(info), 0);	// send # of threads
@@ -223,15 +217,18 @@ int main(void)
 		else
 			for (int j = 0; j < goalLength; j++) 
 				startWord[j] = 126;
-		//printf("endWord = ..%s..\n", startWord);
 		zmq_send(sender, startWord, goalLength + 1, 0);	// send ending word
 
 		zmq_send(sender, theHash, 33, 0);		// send the hash to be matched
 		
 	}
-	
 	printf("waiting for return...\n");
 	int result = zmq_recv(receiver, buff, 255, 0);
+	char *wordResult;
+	wordResult = (char*)malloc(sizeof(char) * result);
+	strncpy(wordResult, buff, result);
+	wordResult[result] = '\0';
+	printf("received end word of: ..%s..", wordResult);
 
 	zmq_close(sender);
 	zmq_close(receiver);
